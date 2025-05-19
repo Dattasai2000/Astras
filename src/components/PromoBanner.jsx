3134ec82d62a0ba80b0d758884cb5f29f5607277
Dattasai <Dattasai2000>
2025-05-19 18:01:56 +0530

    firstcommit

---
+++ src/components/PromoBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PromoBanner() {
+  return (
+    <div>
+        <h1 className='text-2xl bg-red-500 text-center'>PromoBanner</h1>
+    </div>
+  )
+}
